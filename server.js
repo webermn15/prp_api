38a6e649f147854246ce532fc3f8417e20994872
Michael Weber <webermn15@gmail.com>
2019-01-22 08:51:25 -0600

    set up parsing and cors middleware for development, successfully connected to client

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const port = 3000;
 
 const { userDb } = require('./db');
 
-app.get('/', (req, res) => {
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/api', (req, res) => {
+	console.log(req.body);
+
 	userDb.getOneUserById(1)
 		.then(data => res.send({'username': data.rows[0].username}))
 		.catch(err => console.log(err));
